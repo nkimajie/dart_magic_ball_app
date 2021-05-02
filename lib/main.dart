1910ed3e43c20a370d58cf8b5e62ab6fb5294161
Takon Ajie Njar <44798140+nkimajie@users.noreply.github.com>
2021-05-02 22:04:21 +0100

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,46 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          backgroundColor: Colors.teal.shade900,
+          appBar: AppBar(
+            title: Text('Magic App'),
+            backgroundColor: Colors.teal.shade300,
+          ),
+          body: MargicApp(),
+        ),
       ),
     );
+
+
+class MargicApp extends StatefulWidget {
+  @override
+  _MargicAppState createState() => _MargicAppState();
+}
+
+class _MargicAppState extends State<MargicApp> {
+
+  int imageNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: (){
+                setState(() {
+                  imageNumber = Random().nextInt(5) + 1;
+                });
+              },
+              child: Image.asset('images/ball$imageNumber.png'),
+            ),
+          )
+        ]
+      ),
+    );
+  }
+}
+
